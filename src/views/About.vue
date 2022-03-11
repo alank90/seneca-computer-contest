90508dda91d916a7a2e8c6ac28d2e0fed74b7038
Alan K <akillian@outlook.com>
2022-03-11 14:24:12 -0500

    commit

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <router-link to="/">Home</router-link>
+    <p>&#169; 2022 by Alan Killian</p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <h1>Home View</h1>
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from "../../src/components/HelloWorld.vue";
+</script>
