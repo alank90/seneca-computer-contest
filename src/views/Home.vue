f305acab0d90fea09e9fe1dfcf492514431606be
Alan K <akillian@outlook.com>
2022-03-23 13:46:21 -0400

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,15 +21,41 @@
 
       <div class="pure-g">
         <div class="pure-u-1-3">
-          <img src="@/assets/images/card-1.jpg" alt="Auditorium" />
-          <p>Thirds</p>
+          <div class="card-header">
+            <img
+              class="pure-img-responsive"
+              src="@/assets/images/card-1.jpg"
+              alt="Auditorium"
+            />
+          </div>
+          <div class="card-body">
+            <h6 class="card-category text-info">
+              <i class="material-icons">calendar_today</i>When?
+            </h6>
+            <h4 class="card-title">Friday, April 8th 2022</h4>
+            <p class="card-description">Last Afternoon / Early Evening</p>
+            <p>
+              Email <b>matthews@siena.edu</b> if you are interested in
+              participating
+            </p>
+            <p><button class="pure-button btn btn-info">Details</button></p>
+          </div>
+        </div>
+
+        <div class="pure-u-1-3">
+          <img
+            class="pure-img-responsive"
+            src="@/assets/images/card-2.jpg"
+            alt="Church"
+          />
+          <p class="card-description">Thirds</p>
         </div>
         <div class="pure-u-1-3">
-          <img src="@/assets/images/card-2.jpg" alt="Church" />
-          <p>Thirds</p>
-        </div>
-        <div class="pure-u-1-3">
-          <img src="@/assets/images/card-3.jpg" alt="Students" />
+          <img
+            class="pure-img-responsive"
+            src="@/assets/images/card-3.jpg"
+            alt="Students"
+          />
           <p>Thirds</p>
         </div>
       </div>
@@ -38,6 +64,8 @@
 </template>
 
 <style scoped>
+@import "@/assets/css/card.css";
+
 h1,
 h2 {
   color: var(--sienna-yellow);
@@ -53,6 +81,11 @@ h2 > span {
   color: var(--sienna-green);
 }
 
+h4 ~ p {
+  font-size: 14px;
+  margin: 0 0 10px;
+}
+
 img[alt="Sienna logo"] {
   margin: 0 auto;
 }
@@ -63,7 +96,8 @@ img[alt="Sienna logo"] {
   position: relative;
   z-index: 3;
   margin-top: 30px;
-  background-color: #f7f7f5b2;
+  background-color: #e6e9e6b6;
+  padding: 0 50px;
 }
 
 .main-raised {
@@ -78,5 +112,14 @@ img[alt="Sienna logo"] {
   background-position: center center;
   background-size: cover;
   padding: 70px 0;
+}
+
+/* Add a gutter to Pure's Columns */
+.pure-g {
+  margin: 0 -1em;
+}
+
+.pure-g > div {
+  padding: 0 1em;
 }
 </style>
